c768ee490212cb8b57e3341ba90b32c6301bb973
Rich Montero <monterorf@gmail.com>
2021-02-21 19:38:12 -0800

    add country and city controllers

---
+++ Sports.API/Controllers/CityController.cs
@@ -0,0 +1,55 @@
+﻿using DataAccess.Generic;
+using Entities.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sports.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        private readonly IGenericRepository<City> _genericCityRepository;
+        private readonly IGenericRepository<Country> _genericCountryRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CityController(IGenericRepository<City> genericCityRepository, IGenericRepository<Country> genericCountryRepository, IUnitOfWork unitOfWork)
+        {
+            _genericCityRepository = genericCityRepository;
+            _genericCountryRepository = genericCountryRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<City>> Get()
+        {
+            return await _genericCityRepository.GetAsync();
+        }
+
+        [HttpGet("getcities/{countryId}")]
+        public async Task<IEnumerable<City>> Get(int countryId)
+        {
+            return await _genericCityRepository.GetAsync(x => x.CountryId == countryId);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(City city)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var created = await _genericCityRepository.CreateAsync(city);
+
+            if (created)
+                _unitOfWork.Commit();
+
+            return Created("Created", new { Response = StatusCode(201) });
+        }
+    }
+}
---
+++ Sports.API/Controllers/CountryController.cs
@@ -0,0 +1,47 @@
+﻿using DataAccess.Generic;
+using Entities.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sports.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        private readonly IGenericRepository<Country> _genericRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CountryController(IGenericRepository<Country> genericRepository, IUnitOfWork unitOfWork)
+        {
+            _genericRepository = genericRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Country>> Get()
+        {
+            return await _genericRepository.GetAsync();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Country country)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var created = await _genericRepository.CreateAsync(country);
+
+            if (created)
+                _unitOfWork.Commit();
+
+            return Created("Created", new { Response = StatusCode(201) });
+        }
+    }
+}
